815485157111de2242b107ab531db2e99113c178
DaeHyun Kim <rick9707@naver.com>
2022-06-29 22:35:37 +0900

    [ADD] 게시판

--- src/main/java/com/david/webservice/service/PostsService.java
+++ src/main/java/com/david/webservice/service/PostsService.java
@@ -1,11 +1,14 @@
 package com.david.webservice.service;
 
 import com.david.webservice.domain.posts.PostsRepository;
+import com.david.webservice.dto.PostsMainResponseDto;
 import com.david.webservice.dto.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -16,4 +19,12 @@ public class PostsService {
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
     }
+
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/david/webservice/web/webController.java
+++ src/main/java/com/david/webservice/web/webController.java
@@ -1,15 +1,21 @@
 package com.david.webservice.web;
 
+import com.david.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class webController {
 
+    private PostsService postsService;
+
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
